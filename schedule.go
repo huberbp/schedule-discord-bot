b28d7f9aee8d08bf922ce18c81a5252f18c95c26
Benjamin Huber <huberbp@dukes.jmu.edu>
2022-10-14 18:16:33 -0400

    Implemented Boilerplate

--- schedule.go
+++ schedule.go
@@ -1,7 +1,49 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+var BotToken string
+
+func init() {
+	err := godotenv.Load("local.env")
+	if err != nil {
+		fmt.Println("Failed to read environemnt variables.")
+	}
+	BotToken = os.Getenv("DISCORD_BOT_AUTH_KEY")
+}
 
 func main() {
-	discord, err := discordgo.New("Bot " + "authentication token")
+	discord, err := discordgo.New("Bot " + BotToken)
+	if err != nil {
+		fmt.Println("Error creating Discord session,", err)
+		return
+	}
+
+	discord.AddHandler(trackCreation)
+
+	discord.Identify.Intents = discordgo.IntentsGuildScheduledEvents
+
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("Error opening session,", err)
+		return
+	}
+
+	defer discord.Close()
+
+	fmt.Println("Made it this far")
+
+	for true {
+
+	}
+}
+
+func trackCreation(s *discordgo.Session, m *discordgo.GuildScheduledEventCreate) {
+	fmt.Println("Event Created.")
 }
