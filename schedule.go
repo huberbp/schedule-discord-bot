ee6660af75ebf092d8ac38693f9839513e8105b5
Benjamin Huber <huberbp@dukes.jmu.edu>
2022-10-15 22:33:42 -0400

    Implemented logging

--- schedule.go
+++ schedule.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,9 +14,19 @@ import (
 var BotToken string
 
 func init() {
-	err := godotenv.Load("local.env")
+	logFile, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		fmt.Println("Failed to read environemnt variables.")
+		fmt.Println("ERROR", err, "- Failed to open log file for log output.")
+		return
+	}
+	log.SetOutput(logFile)
+	go log.Println("Initializing new session") // According to stack overflow, log is threadsafe
+
+	err = godotenv.Load("local.env")
+	if err != nil {
+		fmt.Println("ERROR", err, "- Failed to read environemnt variables.")
+		log.Println("ERROR", err, "- Failed to read environemnt variables.")
+		return
 	}
 	BotToken = os.Getenv("DISCORD_BOT_AUTH_KEY")
 }
@@ -23,23 +34,28 @@ func init() {
 func main() {
 	discord, err := discordgo.New("Bot " + BotToken)
 	if err != nil {
-		fmt.Println("Error creating Discord session,", err)
+		fmt.Println("ERROR", err, "- Error creating Discord session.")
+		log.Println("ERROR", err, "- Error creating Discord session.")
 		return
 	}
 
+	// Add handlers (trackCreation is a fointer)
 	discord.AddHandler(trackCreation)
 
+	// Setup the intents to send to discord on the initial handshake greeting
 	discord.Identify.Intents = discordgo.IntentsGuildScheduledEvents
 
 	err = discord.Open()
 	if err != nil {
 		fmt.Println("Error opening session,", err)
+		log.Println("Error opening session,", err)
 		return
 	}
 
 	defer discord.Close()
 
 	fmt.Println("App started:", os.Getpid())
+	go log.Println("App started:", os.Getpid())
 
 	// Allocate memory for signal handler
 	stop := make(chan os.Signal, 1)
@@ -49,6 +65,7 @@ func main() {
 	<-stop
 	// Notify user of shutdown
 	fmt.Println("Shut Down")
+	log.Println("Shut Down")
 }
 
 func trackCreation(s *discordgo.Session, m *discordgo.GuildScheduledEventCreate) {
