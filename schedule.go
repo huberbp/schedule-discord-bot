4e079277366157faaa0ec5826b8ce62ae2fd9846
Benjamin Huber <huberbp@dukes.jmu.edu>
2022-10-16 01:16:04 -0400

    Implemented command auto uploading

--- schedule.go
+++ schedule.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -11,10 +12,15 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var BotToken string
+var (
+	BotToken        string
+	logFile         *os.File
+	err             error
+	commandHandlers map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate)
+)
 
 func init() {
-	logFile, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	logFile, err = os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		fmt.Println("ERROR", err, "- Failed to open log file for log output.")
 		return
@@ -32,11 +38,51 @@ func init() {
 }
 
 func main() {
+	defer logFile.Close()
 	discord, err := discordgo.New("Bot " + BotToken)
 	if err != nil {
 		fmt.Println("ERROR", err, "- Error creating Discord session.")
 		log.Println("ERROR", err, "- Error creating Discord session.")
 		return
+	}
+
+	// Check application commands and update them if different
+	// appCommands, err := discord.ApplicationCommands(os.Getenv("DISCORD_APP_ID"), "")
+	// if err != nil {
+	// 	fmt.Println("ERROR", err, "- Error getting app commands.")
+	// 	log.Println("ERROR", err, "- Error getting app commands.")
+	// 	return
+	// }
+
+	directories, err := os.ReadDir("app_commands")
+	if err != nil {
+		fmt.Println("ERROR", err, "- Error reading local command store.")
+		log.Println("ERROR", err, "- Error reading local command store.")
+		return
+	}
+
+	for _, directory := range directories { // all "directories" will be files anyways here
+		var dirName string = directory.Name()
+
+		commandJSON, err := os.ReadFile("app_commands/" + dirName)
+		if err != nil {
+			fmt.Println("ERROR", err, "- Error reading local", dirName, "command.")
+			log.Println("ERROR", err, "- Error reading local", dirName, "command.")
+			return
+		}
+
+		var localApp discordgo.ApplicationCommand
+
+		json.Unmarshal(commandJSON, &localApp)
+
+		// var discordApp discordgo.ApplicationCommand = *appCommands[index]
+
+		_, err = discord.ApplicationCommandCreate(os.Getenv("DISCORD_APP_ID"), "", &localApp)
+		if err != nil {
+			fmt.Println("ERROR", err, "- Error updating", localApp.Name, "command.")
+			log.Println("ERROR", err, "- Error updating", localApp.Name, "command.")
+			return
+		}
 	}
 
 	// Add handlers (trackCreation is a fointer)
